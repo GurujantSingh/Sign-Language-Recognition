8cfdb0462a55738d2888547f8c4319a1a389fce3
Gurujant Singh <gurujant.singh@outlook.com>
2024-04-23 17:31:30 +0530

    Add: added the info

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Import Libraries
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
--- pred.py
+++ pred.py
@@ -1,3 +1,4 @@
+# Import Libraries
 import numpy as np
 import tensorflow as tf
 from sklearn.metrics import classification_report
